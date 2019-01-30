dcab750aa8421a95c405c061a14bcb11b771ca48
Unknown <wbrown026@gmail.com>
2019-01-30 18:23:25 -0500

    Shower Mini Game Half Done

---
+++ Morning Ritual/Assets/Scripts/Shake.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public float shakeDelay = .2f;
+    public float duration;
+    public float magnitude;
+
+    private float count;
+
+    void Start()
+    {
+        count = shakeDelay;
+    }
+
+    void Update()
+    {
+        if (count <= 0.0f)
+        {
+            count = shakeDelay;
+            StartCoroutine(ShakeObject(duration, magnitude));
+        }
+
+        count -= Time.deltaTime;
+    }
+
+    IEnumerator ShakeObject(float dur, float mag)
+    {
+        Vector3 originalPos = transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * mag;
+            float y = Random.Range(-1f, 1f) * mag;
+
+            transform.localPosition = new Vector3(originalPos.x + x, originalPos.y + y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
