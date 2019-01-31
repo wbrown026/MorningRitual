0419a0ae87d2f62822a27ff7e8a1c14e620c8a67
ep429 <pambukue3@gmail.com>
2019-01-30 22:05:26 -0500

    Milk, Cereal, Main Menu

---
+++ Morning Ritual/Assets/Scripts/Breakfast/MilkOrCereal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkOrCereal : MonoBehaviour
+{
+    public GameObject CerealStuff;
+    public GameObject MilkStuff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MilkChoice()
+    {
+        GameObject.Find("MilkOrCereal").SetActive(false);
+        MilkStuff.SetActive(true);
+
+    }
+
+    public void CerealChoice()
+    {
+        GameObject.Find("MilkOrCereal").SetActive(false);
+        GetComponent<SpawnCereal>().enabled = true;
+        CerealStuff.SetActive(true);
+    }
+}
---
+++ Morning Ritual/Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadBreakfast()
+    {
+        SceneManager.LoadScene("Breakfast");
+    }
+
+    public void LoadBrush()
+    {
+        SceneManager.LoadScene("BrushTeeth");
+    }
+
+    public void LoadDress()
+    {
+        SceneManager.LoadScene("GetDressed");
+    }
+
+    public void LoadShower()
+    {
+        SceneManager.LoadScene("Shower");
+    }
+
+    public void LoadWake()
+    {
+        SceneManager.LoadScene("WakeUp");
+    }
+}
